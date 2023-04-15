e04c390025e7ecf7fba7d244e10e27a8b60157dc
Ruben0401 <rubencio_123@hotmail.com>
2023-04-14 21:18:03 -0500

    fix: new link vercel

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    url: 'http://18.223.111.88:5000',
+    url: 'https://proyecto-dss-backend.vercel.app',
+    //url: 'http://18.223.111.88:5000',
     //url: 'ec2-18-223-111-88.us-east-2.compute.amazonaws.com:5000',
     //url:'http://localhost:4000'
 }
